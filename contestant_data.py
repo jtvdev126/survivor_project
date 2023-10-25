32024331d9048959f2f8161a7ada3871ec3ae771
Joel Varughese <jtvdev126@gmail.com>
2023-10-25 14:44:03 -0500

    added contestant data

---
+++ contestant_data.py
@@ -0,0 +1,7 @@
+contestant_data = [
+    {"season": "Season1", "contestant": "Contestant1", "placement": 1, "strategy": "Alliance"},
+    {"season": "Season1", "contestant": "Contestant2", "placement": 2, "strategy": "Challenge Beast"},
+    # Add data for the remaining contestants
+    # ...
+    {"season": "Season10", "contestant": "Contestant30", "placement": 10, "strategy": "Social Game"},
+]
