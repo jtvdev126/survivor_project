88724ef9242946e37bbc3677df044ce0b2d40e1c
Joel Varughese <jtvdev126@gmail.com>
2023-10-25 15:32:13 -0500

    imported matplotlib

--- contestant_data.py
+++ contestant_data.py
@@ -1,6 +1,6 @@
 contestant_data = [
-    {"season": "Season1", "contestant": "Contestant1", "placement": 1, "strategy": "Alliance"},
-    {"season": "Season1", "contestant": "Contestant2", "placement": 2, "strategy": "Challenge Beast"},
+    {"season": "Season44", "contestant": "Contestant1", "placement": 1, "strategy": "Alliance"},
+    {"season": "Season44", "contestant": "Contestant2", "placement": 2, "strategy": "Challenge Beast"},
     # Add data for the remaining contestants
     # ...
     {"season": "Season10", "contestant": "Contestant30", "placement": 10, "strategy": "Social Game"},
@@ -17,3 +17,14 @@ strategy_avg_placement = df.groupby('strategy')['placement'].mean().reset_index(
 strategy_counts = df['strategy'].value_counts().reset_index()
 
 # You can perform more analysis as needed
+
+import matplotlib.pyplot as plt
+
+# Plot average placement by strategy
+plt.figure(figsize=(10, 6))
+plt.bar(strategy_avg_placement['strategy'], strategy_avg_placement['placement'])
+plt.xlabel('Strategy')
+plt.ylabel('Average Placement')
+plt.title('Average Placement by Strategy in Survivor')
+plt.xticks(rotation=45)
+plt.show()
